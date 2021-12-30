99d5ea52f8695543cf91e8fdbbc0b4d71b28c324
cutenam <cutenam@gmail.com>
2021-12-30 10:58:54 +0900

    #2.AUTHENTICATION #2.1 Login Form part One
    1. Auth.js 로그인 정보 입력 부분 작성
     - form, input, submit 관련 리액트 스크립트 코드 작성

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 /**
  * 로그인 인증 처리
@@ -8,5 +8,36 @@ import React from "react";
 
 // export default ()=> <span>Auth</span>;
 // const 로 모듈을 정의해두면, 다른 곳에서 자동 임포트 구문 삽입이 가능하다
-const Auth = ()=> <span>Auth</span>;
+const Auth = ()=>{
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const onChange = (e) =>{
+        console.log(e.target.name);
+        성 = e;
+
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password")  {
+            setPassword(value);
+        }
+    };
+    const onSubmit = (e) => {
+      e.preventDefault();
+    }
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange} name="email" type="text" placeholder="Email" required value={email} />
+                <input onChange={onChange} name="password" type="password" placeholder="Password" required value={password}/>
+                <input type="submit" placeholder="Log In" required />
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
+    )
+}
+
+
 export default Auth;
