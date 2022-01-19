3839d8d3c620ec16660aade40ac1e57a89a38523
cutenam <cutenam@gmail.com>
2022-01-19 14:00:42 +0900

    #2.AUTHENTICATION #2.1 Login Form part One #2.3 Creating Account #2.4 Log In
    1. Auth.js 로그인 정보 입력 부분 작성
     - form, input, submit 관련 리액트 스크립트 코드 작성
     - 입력시 발생하는 이벤트에 의해 폼태그 이름, 값을 가져오는 구문 작성
    2. 파이어베이스 인증 API 호출 사용
    (signInWithEmailAndPassword, createUserWithEmailAndPassword)
     - 이메일, 패스워드 입력하여, 사용자 계정 생성
     - 로그인 상태 여부에 따라 로그인/계정생성 버튼 다르게 보여줌
    3. 앱실행 후, 파이어베이스 모듈 초기화되기까지 시간이 있으므로, 파이어베이스 인증 모듈 이벤트 onAuthStateChanged 사용하여 사용자 로그인 여부 체크 로직 추가
    4. 로그인, 계정생성 사용자가 선택하여 이메일아이디, 패스워드 입력하면, 그에 따라, 인증 로직이 수행되도록 로직 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,4 @@
-// import logo from './logo.svg';
-// import './App.css';
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 // import AppRouter from "./Router";
 // import firebase from "../firebase";  // firebase.js 를 상대경로로 임포트, 절대경로로 임포트하기 위해서 jsconfig.json　파일에 루트 디렉토리를 설정해주면 가능함
 import AppRouter from "components/Router";
@@ -8,6 +6,14 @@ import AppRouter from "components/Router";
 import {firebaseApp, authService} from "firebaseInstance";
 // import authService from "firebaseInstance";
 
+/**
+ * App
+ * @returns {JSX.Element}
+ * @constructor
+ *
+ * 1. 로그인 여부 확인 : authService.currentUser
+ *  - 앱이 실행되어, 파이어베이스 모듈이 실행되는 시간이 걸리므로, 로그인되어있는 사용자 정보를 바로 보여줄 수 있도록 개선 필요 *
+ */
 
 // App.js　에서 어플리케이션의 모든 로직을 핸들링하도록 구조를 잡을거라 함
 function App() {
@@ -15,17 +21,43 @@ function App() {
   // const auth = fbase.auth();
   const auth = authService;
   // 유저를 가져와서 로그인 여부를 판단하도록 하는 것!
-  console.log(auth.currentUser);
+  // console.log(auth.currentUser);
   const authCurrentUser = auth.currentUser;
+  // auth.currentUser : 처음에는 null　값이 찍히고, 시간이 지나야 로그인 정보가 보임
+  // 왜냐하면, 앱실행 후 파이어베이스 연동 시간이 걸리기 때문 => onAuthStateChanged 사용
+  // setInterval(()=>{
+  //     console.log(auth.currentUser);
+  // }, 2000);
+
+  // 파이어베이스 초기화 여부 확인 변수
+  const [init, setInit] = useState(false);
 
   // 로그인 여부 스테이트 -> Router.js에 props로 전달함
-  const [isLoggedIn, setIsLoggedIn] = useState(authCurrentUser);
+  // const [isLoggedIn, setIsLoggedIn] = useState(authCurrentUser);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // onAuthStateChanged : 사용자의 인증 관련(생성, 로그인, 로그아웃등) 이벤트 발생
+  // 로그아웃은 브라우저 indexedDB데이터를 삭제하면 이벤트 발생함
+  // 실제 로그인된 시점을 알 수 있음
+  useEffect(()=>{
+      authService.onAuthStateChanged((user) => {
+          // console.log(user);
+          // 사용자 정보가 있으면 로그인 된것으로,,,
+          if (user) {
+            setIsLoggedIn(true);
+          } else {
+            setIsLoggedIn(false);
+          }
+          // 파이어베이스가 구동된 것으로 함
+          setInit(true);
+      })
+  }, []);
 
   return (
       <>
-   <AppRouter isLoggedIn={isLoggedIn} />
-   {/*<footer>&copy; Nwitter {new Date().getFullYear()}</footer>*/}
+          {/* 파이어베이스 초기화 여부에 따라 메뉴 오픈 여부 처리 */}
+          {init ? <AppRouter isLoggedIn={isLoggedIn}/> : "Initialilizing..."}
+       <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
       </>
 
   );
