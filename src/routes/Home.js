d55312d7fbe58c3a95ca608533a8700e109f6f03
cutenam <cutenam@gmail.com>
2022-01-21 14:27:42 +0900

    #3 NWEETING #3.0 Form and Database Setup
    1. 트윗메시지 작성을 위한 form, 이벤트 함수 작성
     - Home.js
    2. 파이어베이스 콘솔 > 클라우드 파이어베이스 데이터베이스 스토리지 생성, firebaseInstance.js
    　에 데이터베이스 관련 파이어베이스 모듈 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 
 // export default ()=> <span>Home</span>;
-const Home =  ()=> <span>Home</span>;
+// const Home =  ()=> <span>Home</span>;
+/**
+ * 트윗 메시지 입력 기능
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const Home =  () => {
+
+    const [nweet, setNweet] = useState("");
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+    }
+    
+    const onChange = (e) => {
+        const { target : {value}} = e;
+        // console.log(value);
+        setNweet(value);
+    }
+
+    return (
+    <div>
+        <form onSubmit={onSubmit}>
+            <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120}/>
+            <input type="submit" value="Nweet"/>
+        </form>
+    </div>
+    )
+};
 export default Home;
