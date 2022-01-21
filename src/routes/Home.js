98a8be07df074a28cf221848677371c557b1a01e
cutenam <cutenam@gmail.com>
2022-01-21 16:13:14 +0900

    #3 NWEETING #3.0 Form and Database Setup #3.1 Nweeting! #3.2 Getting the Nweets
    1. 트윗메시지 작성을 위한 form, 이벤트 함수 작성
     - Home.js
    2. 파이어베이스 콘솔 > 클라우드 파이어베이스 데이터베이스 스토리지 생성, firebaseInstance.js
    　에 데이터베이스 관련 파이어베이스 모듈 추가,
    버전 9에서 firestore 사용법이 달라짐
     - import { getFirestore } from "firebase/firestore";
    3. 트윗메시지 입력하여, firestore 에 콜렉션 및 도큐먼트 생성하는 로직 추가(Home.js)
     - import {dbService} from "firebaseInstance";
       import {addDoc, collection} from "firebase/firestore";
     - await addDoc(collection(dbService, "컬렉션이름"),
        { 도큐먼트필드:내용,....})
    4. firestore 에서 입력한 도큐먼트를 조회하는 로직 추가(Home.js)
     - import {collection, addDoc, getDocs} from "firebase/firestore";
     - 조회데이터를 위한 스테이트 변수 선언 : const [nweets, setNweets] = useState([]);
     - API 이용 하여 도큐먼트 조회 : const dbNweets = await getDocs(collection(dbService, "nweets"));
     - 조회 데이터 가공 getNweets 함수 : obejct 로 만듬, setNweets 이용
     - useEffect 이용하여 화면 갱신 : getNweets 호출

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,6 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import {dbService} from "firebaseInstance";
+import {collection, addDoc, getDocs, serverTimestamp} from "firebase/firestore";  // v9
 
 // export default ()=> <span>Home</span>;
 // const Home =  ()=> <span>Home</span>;
@@ -9,17 +11,90 @@ import React, {useState} from "react";
  */
 const Home =  () => {
 
-    const [nweet, setNweet] = useState("");
+    const [nweet, setNweet] = useState("");  // 트윗 입력 값 스테이트
+    const [nweets, setNweets] = useState([]); // 조회된 트윗 데이터 스테이트
+    // const getNweets = async() => {
+    //     dbService.collection("nweets".get())
+    // }
+    const getNweets = async() => {
+        const dbNweets = await getDocs(collection(dbService, "nweets"));
+        // dbNweets : querySnapshot 을 리턴함. 구체적인 데이터는 제공하는 속성, 메소드를 이용하여, 다시 추출해야 함
+        // forEach 메소드 활용, data 메소드로 document 데이터를 가져온다
+        // console.log(dbNweets);
+        dbNweets.forEach(document => {
+            // console.log(document.data());
+            // 렌더링하기 편하도록 조회된 데이터를 객체로 만듬
+            const nweetObject = {
+                ...document.data(),
+                id: document.id,
+            }
 
-    const onSubmit = (e) => {
-        e.preventDefault();
+            // 조회된 데이터를 배열형태의 스테이트 변수에 할당함, prev : 현재 보이고있는 트윗
+            // 스프레드연산자를 이용하여 조회된 데이터, 기존 데이터를 합침
+            // setNweets((prev) => [document.data(), ...prev]);
+            // 리엑트에서 스테이트값을 변경시키는 set~함수에는 값만이 아니라, 함수를 이용하여 값을 세팅할 수 있다.
+            // 함수를 전달할때, 이전값 즉, 현재값, 변경되기 이전 값에 접근이 가능함 : prev
+
+            setNweets((prev) => [nweetObject, ...prev]);
+
+        });
     }
-    
+
+    // useEffect(()=> {
+    //     dbService.collection("nweets".get())
+    //
+    // }, []);
+
+    useEffect(()=>{
+        getNweets();
+    }, []);
+
+    /**
+     * 글 입력 시(submit), 파이어베이스 파이어스토어의 도큐먼트가 생성되는 것임...
+     * @param e
+     */
+    // v8
+    // const onSubmit = (e) => {
+    //     e.preventDefault();
+    //     // 파이어스토어에 콜렉션 생성하는 메소드, v8
+    //     dbService.collection("nweets").add({
+    //         nweet: nweet,  // 도큐먼트 키, 스테이트 변수와 맞춤
+    //         // nweet
+    //         createdAt: Date.now(), // 생성날짜
+    //     });
+    //
+    //     setNweet(""); // 트윗작성 후 서브밋하면, 입력 창은 비워둠
+    // }
+    // v9
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        // 파이어스토어에 콜렉션 생성하는 메소드, v8
+        // dbService.collection("nweets").add({
+        //     nweet: nweet,  // 도큐먼트 키, 스테이트 변수와 맞춤
+        //     // nweet
+        //     createdAt: Date.now(), // 생성날짜
+        // });
+
+        // 파이어스토어에 콜렉션 생성하는 메소드, v9
+        try {
+          await addDoc(collection(dbService, "nweets"),
+              {
+                  nweet:nweet,          // 도큐먼트 키, 스테이트 변수와 맞춤
+                  createdAt: Date.now() // 생성날짜
+              })
+        } catch (error) {
+            console.error("Error adding document:", error);
+        }
+
+        setNweet(""); // 트윗작성 후 서브밋하면, 입력 창은 비워둠
+    }
+
     const onChange = (e) => {
         const { target : {value}} = e;
         // console.log(value);
         setNweet(value);
     }
+    console.log(nweets);
 
     return (
     <div>
@@ -27,7 +102,17 @@ const Home =  () => {
             <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120}/>
             <input type="submit" value="Nweet"/>
         </form>
+        <div>
+            {
+                nweets.map(nweet =>
+                    <div key={nweet.id}>
+                        <h4>{nweet.nweet}</h4>
+                    </div>
+                )
+            }
+        </div>
     </div>
+
     )
 };
 export default Home;
