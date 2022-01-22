2ad1c3e49bb7d5bef1dfb3805c4dd863bfdf7fca
cutenam <cutenam@gmail.com>
2022-01-22 15:29:55 +0900

    #3 NWEETING #3.0 Form and Database Setup #3.1 Nweeting! #3.2 Getting the Nweets #3.3 Realtime Nweets #3.4 Delete and Update part One #3.5 Delete and Update part Two
    1. 트윗메시지 작성을 위한 form, 이벤트 함수 작성
     - Home.js
    2. 파이어베이스 콘솔 > 클라우드 파이어베이스 데이터베이스 스토리지 생성, firebaseInstance.js
    　에 데이터베이스 관련 파이어베이스 모듈 추가,
    버전 9에서 firestore 사용법이 달라짐
     - import { getFirestore } from "firebase/firestore";
    3. 트윗메시지 입력하여, firestore 에 콜렉션 및 도큐먼트 생성하는 로직 추가(Home.js)
     - import {dbService} from "firebaseInstance";
       import {addDoc, collection} from "firebase/firestore";
     - await addDoc(collection(dbService, "컬렉션이름"),
        { 도큐먼트필드:내용,....})
    4. firestore 에서 입력한 도큐먼트를 조회하는 로직 추가(Home.js)
     - import {collection, addDoc, getDocs} from "firebase/firestore";
     - 조회데이터를 위한 스테이트 변수 선언 : const [nweets, setNweets] = useState([]);
     - API 이용 하여 도큐먼트 조회 : const dbNweets = await getDocs(collection(dbService, "nweets"));
     - 조회 데이터 가공 getNweets 함수 : obejct 로 만듬, setNweets 이용
     - useEffect 이용하여 화면 갱신 : getNweets 호출
     5. 도큐먼트를 조회 API 변경
     - 기존 : getDocs, forEach 사용, 데이터를 행단위로 읽어들이기때문에, 화면 리렌더링이 일어남
     - 수정 : onSnapshot, 한번에 데이터를 가져와서 렌더링함. 리렌더링 최소화,
     - onSnapshot : 데이터 실시간 변화에 대한 이벤트 발생시켜줌. 실제 데이터 추가, 삭제되는 순간, 화면에 실시간으로 보여줄 수 있다.
     6. 트위터 메시지 리스트 컴포넌트화(Nweet.js)
      - 로그인사용자, 메시지작성자 비교하여, 메시지 수정 삭제를 기능 추가
     7. 파이어클라우드 도큐먼트 수정, 삭제 API　이용(v9)
      - import doc,deleteDoc,updateDoc from "firebase/firestore"
      - 수정 입력화면과 조회화면을 스테이트변수(setEditing) 이용하여 구분하여 보여줌

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,17 +1,103 @@
-import React from "react";
+import React, {useState} from "react";
+import {dbService} from "firebaseInstance";
+import {collection, addDoc, getDocs,
+    doc,
+    deleteDoc,
+    updateDoc,
+    onSnapshot,
+    orderBy,
+    query,
+    where,
+    serverTimestamp} from "firebase/firestore";
 
-const Nweet = ({nweetObj, isOwner}) => (
-    <div>
-        <h4>{nweetObj.text}</h4>
-        {
-            // 본인이 작성한 트윗에서만 삭제, 수정 버튼이 보이도록함
-            isOwner &&
-            <>
-                <button>Delete Nweet</button>
-                <button>Edit Nweet</button>
-            </>
+const Nweet = ({nweetObj, isOwner}) => {
+    // 수정 모드 알려주는 스테이트변수
+    const [editing, setEditing] = useState(false);
+    // 수정할 메시지 스테이트변수, 디폴트 : 현재 메시지 텍스트
+    // 입력된 메시지를 업데이트해줌 : 스테이트변수의 역할
+    const [newNweet, setNewNweet] = useState(nweetObj.text);
+
+
+    // 트윗 삭제 함수
+    // 해당 메시지의 도큐먼트 id (nweetObj.id)를 이용하여 삭제함
+    const onDeleteClick = async () => {
+        const ok = window.confirm("Are you sure want to delete this nweet?");
+        console.log(ok);
+
+       // v8
+       //  if(ok){
+       //      //delete
+       //      await dbService.doc(`nweets/${nweetObj.id}`).delete();
+       //  }
+
+        // v9
+        const nweetDocRef = doc(dbService, "nweets", `${nweetObj.id}`);
+        // console.log(nweetDocRef);
+
+        if(ok) {
+           await deleteDoc(nweetDocRef);
         }
-    </div>
-);
+    }
+
+    const toggleEditing = () => setEditing((prev) => !prev);
+
+    const onChange = (e) => {
+        const {
+            target: {value},
+        } = e;
+        // 스테이트 변수에 입력 태그의 속성과 연결시켜줘야, 입력이 가능함 (리액트 화면 갱신 프로세스)
+        setNewNweet(value);
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        // console.log(nweetObj, newNweet);
+
+        // v8
+        // await dbService.doc(`nweets/${nweetObj.id}`).update({
+        //     text: newNweet,
+        // });
+
+        // v9
+        const nweetDocRef = doc(dbService, "nweets", `${nweetObj.id}`);
+        // console.log(nweetDocRef);
+        await updateDoc(nweetDocRef, {
+            text: newNweet,
+        });
+
+        // 수정모드 원복
+        setEditing(false);
+
+    };
+
+   return (
+       <div>
+           {
+               // 수정 버튼 true/false 에 따라, 입력폼 또는 메시지조회 화면 보여줌
+               editing ?
+                   <>
+                       <form onSubmit={onSubmit}>
+                           <input onChange={onChange} type="text" placeholder="Edit your nweet" value={newNweet} required/>
+                           <input type="submit" value="Update Nweet"/>
+                       </form>
+
+                       <button onClick={toggleEditing}>Cancel</button>
+                   </>
+                   :
+                   <>
+                       <h4>{nweetObj.text}</h4>
+                       {
+                           // 본인이 작성한 트윗에서만 삭제, 수정 버튼이 보이도록함
+                           isOwner &&
+                           <>
+                           <button onClick={onDeleteClick}>Delete Nweet</button>
+                           <button onClick={toggleEditing}>Edit Nweet</button>
+                           </>
+                       }
+                   </>
+           }
+        </div>
+   );
+};
 
 export default Nweet;
